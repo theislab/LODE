804a340d0196642ff0726931c466b96964986bb4
Gottfrid91 <olle.holmberg19@gmail.com>
2020-08-11 12:04:21 +0200

    paths

--- feature_statistics/config.py
+++ feature_statistics/config.py
@@ -1,6 +1,7 @@
 import sys
-
-PROJ_DIR = "/home/icb/olle.holmberg/projects/LODE"
+import os
+from pathlib import Path
+PROJ_DIR = Path(os.getcwd()).parent
 DATA_DIR = "/storage/groups/ml01/datasets/projects/20181610_eyeclinic_niklas.koehler/clinical_feature_segmentation/segmented_volumes20200728"
 WORK_SPACE = '/storage/groups/ml01/workspace/olle.holmberg/LODE'
 
