b5606e76cc1743903ff1e19b1f2458a0bfc4c975
Gottfrid91 <olle.holmberg19@gmail.com>
2020-09-28 12:20:20 +0200

    adding conversion to volumetric statistics from pixel statistics

--- feature_statistics/calculate_segmentation_statistics.py
+++ feature_statistics/calculate_segmentation_statistics.py
@@ -1,5 +1,5 @@
 import os
-from config import PROJ_DIR, SEG_DIR, WORK_SPACE
+from config import PROJ_DIR, SEG_DIR, WORK_SPACE, OCT_DIR
 from feature_statistics.utils.etdrs_utils import ETDRSUtils
 import pandas as pd
 from tqdm import tqdm
@@ -11,27 +11,25 @@ inputs = tqdm(os.listdir(SEG_DIR))
 print(f"number of cores {num_cores} set to paralell process")
 feature_pd = None
 
-'''
-
 if __name__ == "__main__":
-    filename = "709_20160808_L_482412001.npy"
+    search_filename = "93552_20131118_L_300449001.npy"
     for i, filename in tqdm(enumerate(os.listdir(SEG_DIR))):
-        try:
-            etdrs = ETDRSUtils(path=os.path.join(SEG_DIR, filename))
+        if filename == search_filename:
+            #try:
+            etdrs = ETDRSUtils(path=os.path.join(SEG_DIR, filename), dicom_path = OCT_DIR)
             feature_log = etdrs.get_etdrs_stats()
-    
+
             if feature_pd is None:
                 feature_pd = pd.DataFrame(feature_log, index = [i])
             else:
                 feature_pd.append(pd.DataFrame(feature_log, index = [i]))
-        except:
-            print("record not working, skippint record: ", filename)
-            continue
+            #except:
+            #    print("record not working, skippint record: ", filename)
+            #    continue
 
     feature_pd.to_csv(os.path.join(WORK_SPACE, "segmentation_statistics.csv"))
 
 '''
-
 def process(i):
     try:
         etdrs = ETDRSUtils(path = os.path.join(SEG_DIR, i))
@@ -40,9 +38,9 @@ def process(i):
     except:
         print("record not working, skippint record: ", i)
 
-
 if __name__ == "__main__":
     processed_list = Parallel(n_jobs = num_cores)(delayed(process)(i) for i in inputs)
     features_pd = pd.DataFrame.from_dict(processed_list)
     features_pd.to_csv(os.path.join(WORK_SPACE, "segmentation_statistics.csv"))
+'''
 
