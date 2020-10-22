04b92c602a9955445215e6296bb96dd1648c7c28
Gottfrid91 <olle.holmberg19@gmail.com>
2020-10-22 20:57:19 +0200

    camera effect load

--- feature_segmentation/generators/generator_utils/image_processing.py
+++ feature_segmentation/generators/generator_utils/image_processing.py
@@ -1,5 +1,23 @@
 import numpy as np
 from PIL import Image
+import cv2
+from random import random;
+
+
+def invert_camera_effect(img):
+    # annotate camera artifact
+    if len(img.shape) == 3:
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    gray = img
+    ret, mask = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY)
+    kernel = np.ones((20, 20), np.uint8)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+
+    img[mask[:, :] == 0] = 255
+
+    return np.stack((img,) * 3, axis = -1)
 
 
 def resize(im, shape):
@@ -18,6 +36,32 @@ def resize(im, shape):
     new_im.paste(im, ((desired_size - new_size[0]) // 2,
                       (desired_size - new_size[1]) // 2))
     return np.array(new_im)
+
+
+def read_resize_random_invert(img_path, label_path, shape):
+    # load samples
+    im = Image.open(img_path)
+    lbl = Image.open(label_path)
+
+    # convert to numpy
+    im = np.array(im)
+    lbl = np.array(lbl)
+
+    if random() < 0.2:
+        im = invert_camera_effect(im)
+
+    # resize samples
+    im_resized = resize(im, shape)
+    lbl_resized = resize(lbl, shape)
+
+    # if image grey scale, make 3 channel
+    if len(im_resized.shape) == 2:
+        im_resized = np.stack((im_resized,) * 3, axis = -1)
+
+    # Store sample
+    image = im_resized.reshape(shape[0], shape[1], 3)
+    label = lbl_resized.reshape((shape[0], shape[1], 1))
+    return image, label
 
 
 def read_resize(img_path, label_path, shape):
