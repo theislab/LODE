a9ce6d842e5cac0c78560cf963d5dd4fce04ecdd
Olle Holmberg <olleholmberg19@gmail.com>
2020-10-24 17:43:08 +0200

    bef pull

--- feature_segmentation/config.py
+++ feature_segmentation/config.py
@@ -9,12 +9,12 @@ EMBEDD_SAVE_PATH: path where to save embeddings
 """
 
 
-PROJ_DIR = '/home/icb/olle.holmberg/projects/LODE'
-WORK_SPACE = '/storage/groups/ml01/workspace/olle.holmberg/LODE'
+PROJ_DIR = '/home/olle/PycharmProjects/LODE'
+WORK_SPACE = '/home/olle/PycharmProjects/LODE/workspace'
 VOL_SAVE_PATH = os.path.join(WORK_SPACE, "feature_segmentation/segmentation/segmented_volumes20200822")
 EMBEDD_DIR = os.path.join(WORK_SPACE, "feature_segmentation/segmentation/embeddings20200822")
 TRAIN_DATA_PATH = os.path.join(WORK_SPACE, "feature_segmentation/segmentation/data/train_data/hq_examples")
-#TRAIN_DATA_PATH = os.path.join(WORK_SPACE, "feature_segmentation/segmentation/data/train_data/first_examples")
+TRAIN_DATA_PATH = os.path.join(WORK_SPACE, "feature_segmentation/segmentation/data/train_data/first_examples")
 
 OCT_DIR = "/storage/groups/ml01/datasets/raw/2018_LMUAugenklinik_niklas.koehler/Studies/Optical Coherence Tomography Scanner"
 
