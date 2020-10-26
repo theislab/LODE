221968293b5838f383db27d147bbcf2f7849514f
Gottfrid91 <olle.holmberg19@gmail.com>
2020-10-26 11:38:04 +0100

    adding new evaluation scripts

---
+++ feature_segmentation/evaluate/evaluation_ensemble.py
@@ -0,0 +1,63 @@
+import os
+from feature_segmentation.evaluate.evaluation_utils import get_result_report, load_test_config, \
+    ensemble_predict, check_enseble_test_ids
+from feature_segmentation.generators.generator_utils.image_processing import read_resize
+from feature_segmentation.segmentation_config import WORK_SPACE, TRAIN_DATA_PATH
+from feature_segmentation.utils.plotting import plot_image_label_prediction, plot_uncertainty_heatmaps, \
+    plot_uncertainty_statistics
+
+# select model to be evaluated
+models_directory = os.path.join(WORK_SPACE, "models")
+ensemble_models = ["49", "48"]
+
+ensemble_name = "test_ensemble"
+ensemble_dir = os.path.join(WORK_SPACE, ensemble_name)
+
+if not os.path.exists(ensemble_dir):
+    os.makedirs(ensemble_dir, exist_ok = True)
+
+
+ensemble_dict = {}
+
+# get ensemble config dict
+for ensemble_model in ensemble_models:
+    model_path = os.path.join(models_directory, ensemble_model)
+
+    # load test configurations
+    model, test_ids, params = load_test_config(model_path)
+
+    ensemble_dict[ensemble_model] = {"model": model, "test_ids": test_ids, "params": params}
+
+# assert correct train test split across ensemble
+check_enseble_test_ids(ensemble_dict)
+
+all_predictions = []
+all_labels = []
+all_uq_maps = {}
+
+image_iter = 0
+for i in range(0, len(test_ids) - 1):
+    img_path = os.path.join(TRAIN_DATA_PATH, "images", test_ids[i])
+    label_path = os.path.join(TRAIN_DATA_PATH, "masks", test_ids[i])
+
+    img, lbl = read_resize(img_path, label_path, (params.img_shape, params.img_shape))
+
+    # pre process (255. divide) as when training
+    img = img / 255.
+
+    # run prediction for each ensemble model
+    model_predictions, ensemble_prediction, uq_map = ensemble_predict(ensemble_dict, img)
+
+    all_predictions.extend(ensemble_prediction.flatten().tolist())
+    all_labels.extend(lbl.flatten().tolist())
+    all_uq_maps[test_ids[i]] = uq_map
+
+    # plot all images and their labels/predictions
+    plot_image_label_prediction([img, lbl, ensemble_prediction], ensemble_dir, mode = "test", filename = test_ids[i])
+
+# plot all uq maps
+plot_uncertainty_heatmaps(all_uq_maps, ensemble_dir)
+plot_uncertainty_statistics(all_uq_maps, ensemble_dir)
+
+# generate results files from all prediction and labels and save to model directory
+get_result_report(all_labels, all_predictions, ensemble_dir)
