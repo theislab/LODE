995584a2b5b31a5d70694118525be8bba62a0477
Gottfrid91 <olle.holmberg19@gmail.com>
2020-11-28 14:18:42 +0100

    adding step and exp decay lr function and parameter to params

--- feature_segmentation/generators/generator_utils/oct_augmentations.py
+++ feature_segmentation/generators/generator_utils/oct_augmentations.py
@@ -7,29 +7,24 @@ from albumentations import (
     Compose,
     Rotate,
     ISONoise,
-    GlassBlur,
-    OneOf, RandomSizedCrop, PadIfNeeded, VerticalFlip, RandomRotate90, GridDistortion, CLAHE, RandomBrightness,
-    CenterCrop, JpegCompression, RandomContrast)
+    OneOf, RandomSizedCrop, PadIfNeeded, VerticalFlip, RandomRotate90, GridDistortion, CLAHE)
 
 
 def get_augmentations(params):
     augmentations = {
         "light": Compose([
-            HorizontalFlip(p = params.flip)]),
+            HorizontalFlip(p = 0.5)]),
 
         "medium": Compose([
             OneOf([RandomSizedCrop(min_max_height = (210, 210), height = 256, width = 256, p = 0.1),
                    PadIfNeeded(min_height = 256, min_width = 256, p = 0.5)], p = 1),
             VerticalFlip(p = 0.2),
             RandomRotate90(p = 0.2),
-            # ElasticTransform(p = 0.2),
             Rotate(p = 0.5),
             RandomBrightnessContrast(brightness_limit = 0.6, contrast_limit = 0.6, p = 0.1),
             GaussNoise(p = 0.1, var_limit = (10.0, 25.0)),
             ISONoise(color_shift = (0.01, 0.5), intensity = (0.1, 0.9), p = 0.1),
-            # GlassBlur(sigma = 0.2, max_delta = 1, p = 0.2),
             RandomGamma(gamma_limit = (50, 150), p = 0.1)],
-            # JpegCompression(quality_lower = 39, quality_upper = 100, p = 0.2)
         ),
 
         "severe": Compose([
