20c3b1a4dbaba1f69aa1382736389cc31bb00418
Gottfrid_91 <olleholmberg19@gmail.com>
2020-11-28 15:13:12 +0100

    adding oct aug corection

--- feature_segmentation/generators/generator_utils/oct_augmentations.py
+++ feature_segmentation/generators/generator_utils/oct_augmentations.py
@@ -20,7 +20,7 @@ def get_augmentations(params):
                                    height = params.img_shape,
                                    width = params.img_shape,
                                    p = 0.1),
-                   PadIfNeeded(min_height = params.img_shape, min_width = params.img_shape, p = 0.5)], p = 1),
+                   PadIfNeeded(min_height = params.img_shape, min_width = params.img_shape, p = 1)], p = 1),
             
             VerticalFlip(p = 0.2),
             RandomRotate90(p = 0.2),
@@ -30,4 +30,5 @@ def get_augmentations(params):
             ISONoise(color_shift = (0.01, 0.5), intensity = (0.1, 0.9), p = 0.1),
             RandomGamma(gamma_limit = (50, 150), p = 0.1)],
         )
+        }
     return augmentations
