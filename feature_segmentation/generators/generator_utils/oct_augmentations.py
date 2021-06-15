972eff616abff231efb20371d488e665719fd50d
Gottfrid91 <olle.holmberg19@gmail.com>
2021-06-15 08:45:03 +0200

    bef pull

--- feature_segmentation/generators/generator_utils/oct_augmentations.py
+++ feature_segmentation/generators/generator_utils/oct_augmentations.py
@@ -16,12 +16,14 @@ def get_augmentations(params):
             Rotate(p = 0.5)])
         ,
 
-        "medium": Compose([HorizontalFlip(p=0.5),
-                           RandomBrightnessContrast(p=0.2, brightness_limit=(-0.3, 0.2), contrast_limit=(-0.2, 0.2)),
-                           Downscale(p=1.0, scale_min=0.6, scale_max=0.6)
-                           GaussianBlur(p=0.2, blur_limit=(3,3), sigma_limit=2)
-                           Rotate(p=0.2, interpolation=cv2.INTER_NEAREST)
-                            ShiftScaleRotate(p=0.2, shift_limit=0.2, rotate_limit=0,interpolation=cv2.INTER_NEAREST,
-                                             border_mode=cv2.BORDER_CONSTANT, value=0)],
-        )}
+        "medium": Compose([HorizontalFlip(p = 0.5),
+                           RandomBrightnessContrast(p = 0.2, brightness_limit = (-0.3, 0.2),
+                                                    contrast_limit = (-0.2, 0.2)),
+                           Downscale(p = 1.0, scale_min = 0.6, scale_max = 0.6),
+                           GaussianBlur(p = 0.2, blur_limit = (3, 3), sigma_limit = 2),
+                           Rotate(p = 0.2, interpolation = cv2.INTER_NEAREST),
+                           ShiftScaleRotate(p = 0.2, shift_limit = 0.2, rotate_limit = 0,
+                                            interpolation = cv2.INTER_NEAREST,
+                                            border_mode = cv2.BORDER_CONSTANT, value = 0)],
+                          )}
     return augmentations
