a9ab939eee7f4e35086a691bd8887403e5bf6071
Olle Holmberg <olleholmberg19@gmail.com>
2021-06-29 08:21:15 +0200

    adding feature stat notebooks

--- feature_segmentation/train_cv.py
+++ feature_segmentation/train_cv.py
@@ -133,5 +133,5 @@ if __name__ == "__main__":
     for j in range(NUM_ENSEMBLES):
         for i in range(NUM_OCTS):
             flags.cfs_cv_iteration = i
-            flags.save_model_dir = f"/media/olle/3DCPC/oct_segmentation/cross_validation_runs/logs_{j}"
+            flags.save_model_dir = f"./cv_logs2/logs_{j}"
             main(flags)
